9b192ff85d847e09c219365dd9f4703b5b01d4e0
ELOUAIZI AYOUB <ayoubelouaizi@gmail.com>
2023-03-06 21:06:21 +0000

    cv

---
+++ Projects/CV/cv3/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>My CV</title>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="style.css" />
+</head>
+
+<body>
+    <header>
+        <h1>My Name</h1>
+        <p>Job Title</p>
+    </header>
+
+    <section id="contact">
+        <h2>Contact Information</h2>
+        <ul>
+            <li>Email: myemail@example.com</li>
+            <li>Phone: 555-1234</li>
+            <li>Website: www.example.com</li>
+        </ul>
+    </section>
+
+    <section id="education">
+        <h2>Education</h2>
+        <ul>
+            <li>Degree Name, Major, University, Graduation Date</li>
+            <li>Degree Name, Major, University, Graduation Date</li>
+        </ul>
+    </section>
+
+    <section id="experience">
+        <h2>Experience</h2>
+        <ul>
+            <li>Job Title, Company, Start Date - End Date</li>
+            <li>Job Title, Company, Start Date - End Date</li>
+        </ul>
+    </section>
+
+    <section id="skills">
+        <h2>Skills</h2>
+        <ul>
+            <li>Skill 1</li>
+            <li>Skill 2</li>
+            <li>Skill 3</li>
+        </ul>
+    </section>
+</body>
+
+</html>
