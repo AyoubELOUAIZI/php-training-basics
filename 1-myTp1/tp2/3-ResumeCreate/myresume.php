16b609b319f489aba5330605f9dde774f72dc84f
ELOUAIZI AYOUB <ayoubelouaizi@gmail.com>
2023-03-07 17:04:14 +0000

    finish cv as the teacher want

--- 1-myTp1/tp2/3-ResumeCreate/myresume.php
+++ 1-myTp1/tp2/3-ResumeCreate/myresume.php
@@ -21,6 +21,35 @@ setcookie('visits', $visits, time() + 3600 * 24);
 <?php
 
 
+ if ($_SERVER['REQUEST_METHOD'] === 'POST' ) {
+    if(isset($_POST['Personal_Data']) ) {
+    $personalDetails =array();
+if (file_exists('etatcivil.txt')) {
+    $file = fopen('etatcivil.txt', 'r');
+    while (($personaldetail = fgets($file)) !== false) {
+        $personalDetails[] = $personaldetail;
+    }
+    fclose($file);
+}
+}
+ }
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' ) {
+    if(isset($_POST['Education'])) {
+    $personalDetails =array();
+if (file_exists('formation.txt')) {
+    $file = fopen('formation.txt', 'r');
+    while (($personaldetail = fgets($file)) !== false) {
+        $personalDetails[] = $personaldetail;
+    }
+    fclose($file);
+}
+}
+}
+//---------------------------------------------------------//
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' ) {
+    if(isset($_POST['Experience'])) {
 $experiences = array();
 if (file_exists('experience.txt')) {
     $file = fopen('experience.txt', 'r');
@@ -30,6 +59,22 @@ if (file_exists('experience.txt')) {
     }
     fclose($file);
 }
+}
+}
+
+
+ if ($_SERVER['REQUEST_METHOD'] === 'POST' ) {
+    if(isset($_POST['Hobbies'])) {
+    $personalDetails =array();
+if (file_exists('hobbies.txt')) {
+    $file = fopen('hobbies.txt', 'r');
+    while (($personaldetail = fgets($file)) !== false) {
+        $personalDetails[] = $personaldetail;
+    }
+    fclose($file);
+}
+}
+ }
 
 
 ?>
@@ -58,18 +103,61 @@ if (file_exists('experience.txt')) {
     <div class="container">
         <nav>
             <ul>
-                <li><a href="etatcivil.php">Personal Data</a></li>
-                <li><a href="formation.php">Education</a></li>
-                <li><a href="experience.php">Experience</a></li>
-                <li><a href="hobbies.php">Hobbies</a></li>
+                <li>
+                    <form method="post" action="">
+                        <input type="hidden" name="Personal Data" value="Personal Data">
+                        <input type="submit" value="Personal Data">
+                    </form>
+                </li>
+                <li>
+                    <form method="post" action="">
+                        <input type="hidden" name="Education" value="Education">
+                        <input type="submit" value="Education">
+                    </form>
+
+                </li>
+                <li>
+                    <form method="post" action="">
+                        <input type="hidden" name="Experience" value="Experience">
+                        <input type="submit" value="Experience">
+                    </form>
+
+                </li>
+                <li>
+                    <form method="post" action="">
+                        <input type="hidden" name="Hobbies" value="Hobbies">
+                        <input type="submit" value="Hobbies">
+                    </form>
+
+                </li>
+
+
+
+
+
+
             </ul>
         </nav>
         <div class="cvcontainer">
             <div class="cv">
                 <!-- <h1>cv</h1> -->
+
+
+                <!-- <h2>Details</h2> -->
+                <?php if (!empty($personalDetails)) { ?>
+                <ul>
+                    <?php foreach ($personalDetails as $personalDetail) { ?>
+                    <li>
+                        <?php echo $personalDetail; ?>
+
+                    </li>
+                    <?php } ?>
+                </ul>
+                <?php } ?>
+                <!--  -->
                 <!-- Experienes -->
                 <?php if (!empty($experiences)) { ?>
-                <h2>Experiences</h2>
+                <!-- <h2>Experiences</h2> -->
                 <ul>
                     <?php foreach ($experiences as $experience) { ?>
                     <li>
@@ -84,10 +172,12 @@ if (file_exists('experience.txt')) {
 
                 <!--  -->
             </div>
-            <form method="post" action="connection.php">
-                <input type="hidden" name="logout_token" value="12345">
-                <input type="submit" value="Logout">
-            </form>
+            <div class="logout">
+                <form method="post" action="connection.php">
+                    <input type="hidden" name="logout_token" value="12345">
+                    <input type="submit" value="Logout">
+                </form>
+            </div>
         </div>
     </div>
 
